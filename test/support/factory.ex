c4121020d95d7b611c3e722712d34b35cf10ef9d
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-11 12:05:47 +0100

    Remove unused aliases

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,7 +5,7 @@ defmodule QuizBuzz.Factory do
   Test helpers to build quizzes and associated structs.
   """
 
-  alias QuizBuzz.Quizzes.{Player, Quiz, RandomIDGenerator, Team}
+  alias QuizBuzz.Quizzes.{Quiz, RandomIDGenerator}
 
   def new_quiz do
     Quiz.new(&RandomIDGenerator.next/0)
