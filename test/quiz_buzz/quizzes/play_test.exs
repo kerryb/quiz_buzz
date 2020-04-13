8c0fbbe53329b75bcf57c5a0b018cc94659c1e89
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-13 11:53:24 +0100

    Simplify test

--- test/quiz_buzz/quizzes/play_test.exs
+++ test/quiz_buzz/quizzes/play_test.exs
@@ -12,23 +12,16 @@ defmodule QuizBuzz.Quizzes.PlayTest do
       jane_doe = Player.new("Jane Doe")
       joe_bloggs = Player.new("Joe Bloggs")
       quiz = active_quiz() |> with_player(jane_doe) |> with_player(joe_bloggs)
-      {:ok, quiz: quiz, jane_doe: jane_doe, joe_bloggs: joe_bloggs}
+      {:ok, quiz: quiz, jane_doe: jane_doe}
     end
 
-    test "marks the player as having buzzed", %{quiz: quiz, jane_doe: jane_doe} do
+    test "marks the correct player as having buzzed", %{quiz: quiz, jane_doe: jane_doe} do
       quiz = Play.buzz(quiz, jane_doe)
-      jane_doe = Enum.find(quiz.players, &(&1.name == jane_doe.name))
-      assert jane_doe.buzzed?
-    end
 
-    test "does not mark other players as having buzzed", %{
-      quiz: quiz,
-      jane_doe: jane_doe,
-      joe_bloggs: joe_bloggs
-    } do
-      quiz = Play.buzz(quiz, jane_doe)
-      joe_bloggs = Enum.find(quiz.players, &(&1.name == joe_bloggs.name))
-      refute joe_bloggs.buzzed?
+      assert Enum.map(quiz.players, &{&1.name, &1.buzzed?}) == [
+               {"Joe Bloggs", false},
+               {"Jane Doe", true}
+             ]
     end
   end
 end
