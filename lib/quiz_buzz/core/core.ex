69bf0e10849fe1022cfc4ffbdbb2cdee00b8d809
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-13 16:42:05 +0100

    Add missing delegates to Core API module

--- lib/quiz_buzz/core/core.ex
+++ lib/quiz_buzz/core/core.ex
@@ -3,9 +3,14 @@ defmodule QuizBuzz.Core do
   Entry point for the Core context.
   """
 
-  alias QuizBuzz.Core.Setup
+  alias QuizBuzz.Core.{Play, Setup}
 
+  defdelegate new_quiz, to: Setup
   defdelegate add_team(quiz, name), to: Setup
   defdelegate join_quiz(quiz, name), to: Setup
   defdelegate join_team(quiz, team, player), to: Setup
+  defdelegate start(quiz), to: Setup
+
+  defdelegate buzz(quiz, player), to: Play
+  defdelegate reset_buzzers(quiz), to: Play
 end
