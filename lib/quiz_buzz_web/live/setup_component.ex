504d17cac669f77be841167579edaf5b37f8f9dc
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-19 15:04:53 +0100

    Extract quiz setup (player & teams) to a component

---
+++ lib/quiz_buzz_web/live/setup_component.ex
@@ -0,0 +1,8 @@
+defmodule QuizBuzzWeb.SetupComponent do
+  @moduledoc """
+  Stateless LiveView component for the setup view of a quiz, shared between the
+  quiz and quizmaster views.
+  """
+
+  use Phoenix.LiveComponent
+end
