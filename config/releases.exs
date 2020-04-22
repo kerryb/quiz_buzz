ac0d03333709ce11508cfca5e03235cecd8f32cb
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-22 16:04:24 +0100

    Add localhost to check_origin list

--- config/releases.exs
+++ config/releases.exs
@@ -4,4 +4,4 @@ config :quiz_buzz, QuizBuzzWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "quizbuzz.kerryb.org", port: 443],
-  check_origin: ["//quizbuzz.kerryb.org", "//quizbuzz.gigalixirapp.com"]
+  check_origin: ["//localhost", "//quizbuzz.kerryb.org", "//quizbuzz.gigalixirapp.com"]
