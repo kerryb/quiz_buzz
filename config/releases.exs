2083662deeaf0c1860e692db4bc3609aac0627d8
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-22 15:55:09 +0100

    Add host to release endpoint http config

--- config/releases.exs
+++ config/releases.exs
@@ -2,6 +2,6 @@ import Config
 
 config :quiz_buzz, QuizBuzzWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}],
+  http: [host: "quizbuzz.kerryb.org", port: {:system, "PORT"}],
   url: [scheme: "https", host: "quizbuzz.kerryb.org", port: 443],
   check_origin: ["//quizbuzz.kerryb.org", "//quizbuzz.gigalixirapp.com"]
