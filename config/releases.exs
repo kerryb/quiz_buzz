3bbb525837c40205fbd376d421bab726c6cc4c8b
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-22 15:48:52 +0100

    Revert and tweak prod/release endpoint config

--- config/releases.exs
+++ config/releases.exs
@@ -2,4 +2,6 @@ import Config
 
 config :quiz_buzz, QuizBuzzWeb.Endpoint,
   server: true,
-  http: [host: "quizbuzz.kerryb.org", port: {:system, "PORT"}]
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "quizbuzz.kerryb.org", port: 443],
+  check_origin: ["//quizbuzz.kerryb.org", "//quizbuzz.gigalixirapp.com"]
