8606544714d6c18b0020efb924a9526653037a8c
Kerry Buckley <kerryjbuckley@gmail.com>
2022-12-28 19:17:48 +0000

    Fix cancel button in player name modal

--- lib/quiz_buzz_web/live/quiz_live.ex
+++ lib/quiz_buzz_web/live/quiz_live.ex
@@ -5,16 +5,20 @@ defmodule QuizBuzzWeb.QuizLive do
     {:ok, assign(socket, name: nil, edit_name?: false)}
   end
 
-  def handle_event("save-name", %{"name" => name}, socket) do
-    {:noreply,
-     socket |> assign(name: name, edit_name?: false) |> push_event("save-name", %{name: name})}
-  end
-
   def handle_event("load-name", %{"name" => name}, socket) do
     {:noreply, assign(socket, name: name)}
   end
 
   def handle_event("edit-name", _params, socket) do
     {:noreply, assign(socket, edit_name?: true)}
+  end
+
+  def handle_event("cancel-edit-name", _params, socket) do
+    {:noreply, assign(socket, edit_name?: false)}
+  end
+
+  def handle_event("save-name", %{"name" => name}, socket) do
+    {:noreply,
+     socket |> assign(name: name, edit_name?: false) |> push_event("save-name", %{name: name})}
   end
 end
